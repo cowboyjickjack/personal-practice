de534cb9a4b69a41e343c9f3d6953adde65ad117
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-30 09:57:23 -0500

    finished fourth todo

--- js/loops-practice.js
+++ js/loops-practice.js
@@ -17,9 +17,33 @@ const  findTheSign = (x, y, z) => {
 
 // TODO: Figuring out how to loop with jQuery inputs
 
-let test = document.getElementById('test');
-let htmlString = '';
-for (let i = 1; i <= 5; i++) {
-    htmlString += `<div class="test">hello</div>`;
-}
-test.innerHTML = htmlString;
+// let test = document.getElementById('test');
+// let htmlString = '';
+// for (let i = 1; i <= 5; i++) {
+//     htmlString += `<div class="test">hello</div>`;
+// }
+// test.innerHTML = htmlString;
+
+// TODO: Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result.
+// Sample numbers : 0, -1, 4
+function sortThreeNumbers (x, y, z) {
+    if (x>y && x>z) {
+        if (y>z) {
+            console.log(x + ", " + y + ", " +z);
+        } else {
+            console.log(x + ", " + z + ", " +y);
+        }
+    } else if (y>x && y >z) {
+        if (x>z) {
+            console.log(y + ", " + x + ", " +z);
+        } else {
+            console.log(y + ", " + z + ", " +x);
+        }
+    } else if (z>x && z>y) {
+        if (x>y) {
+            console.log(z + ", " + x + ", " +y);
+        } else {
+            console.log(z + ", " + y + ", " +x);
+        }
+    }
+};
