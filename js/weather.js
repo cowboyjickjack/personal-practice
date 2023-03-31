4d706c324983539d25693bc1b7acf919978bc87c
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-30 19:41:09 -0500

    fixed map html

--- js/weather.js
+++ js/weather.js
@@ -39,8 +39,17 @@ export const weatherForecast = async () => {
 
 
 /** EVENT LISTENERS **/
-document.getElementById('setMarkerButton').addEventListener('click', event => {
-    // prevents form from submitting by clicking button, and resets its function
-    event.preventDefault();
-
-});
+// document.getElementById('setMarkerButton').addEventListener('click', event => {
+//     // prevents form from submitting by clicking button, and resets its function
+//     event.preventDefault();
+//     // map.setZoom(document.getElementById('setMarker').value);
+//
+//     const address = document.getElementById('setMarker').value;
+//     console.log(address);
+//     // geocode sets coordinates, .then stashes the coordinates as 'coords'
+//     geocode(address, MAPBOX_EXERCISE_TOKEN).then(coords=> {
+//         const newMarker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+//         // this centers the map on the input coords
+//         map.setCenter(coords);
+//
+// });
